f977c3e559ed7cb36fccf78d498d05d373bb6347
AmineBouzid007 <stikky.stickers.shop@gmail.com>
2026-02-21 16:39:42 +0100

    Update convoy_config.lua

--- convoy_config.lua
+++ convoy_config.lua
@@ -1,5 +1,27 @@
-Convoy = {}
+Config = Config or {}
 
-Convoy.MaxMembers = 4
-Convoy.CodeLength = 4
-Convoy.UseDatabase = true
+Config.JobPool = {
+    { 
+        name = "Fuel Delivery", 
+        type = "fuel", 
+        img = "images/trailers/tanker.png", 
+        basePrice = 500, 
+        minLevel = 1 
+    },
+    { 
+        name = "Logging Transport", 
+        type = "logs", 
+        img = "images/trailers/trailers3.png", 
+        basePrice = 750, 
+        minLevel = 2 
+    },
+    { 
+        name = "Industrial Containers", 
+        type = "containers", 
+        img = "images/trailers/docktrailer.png", 
+        basePrice = 1200, 
+        minLevel = 3 
+    },
+}
+
+Config.MaxDailyJobs = 5 -- How many random jobs to show in the list
